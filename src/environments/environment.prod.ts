23622b7f393076f810a9931a0ebfd75c2cf10e1a
Brandon Curtis <Brandcurtis43@gmail.com>
2021-04-16 12:09:10 -0600

    gh-pages prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: { 
+    apiKey: "AIzaSyDILRsunz51Dzf-qUcnRgdidxy93Q-YynY",
+    authDomain: "golfscorecard-33b1d.firebaseapp.com",
+    projectId: "golfscorecard-33b1d",
+    storageBucket: "golfscorecard-33b1d.appspot.com",
+    messagingSenderId: "39822647222",
+    appId: "1:39822647222:web:a0cbc5d2e00af4a9942b17"
+  }
 };
